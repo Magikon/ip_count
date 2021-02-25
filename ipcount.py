29a84fbc9973c1edb97bf70869448c312dfed7c7
mikayel <admin@zzillasoft.com>
2021-02-25 17:04:59 +0400

    improvement 2

--- ipcount.py
+++ ipcount.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 # creator Magikon, mikayel.galyan@gmail.com
 """find requested ip's count"""
-import re           # regular expression
-import fileinput    # open large files
-import argparse     # arguments parser
-import gzip         # open gzip files
-import time         # time operations
-
-def findRe(strfind, strre=r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'):
-    """create pattern and search it in given line"""
-    if isinstance(strfind, (bytes, bytearray)): strfind = strfind.decode('UTF-8')
-    temp = re.compile(strre).search(strfind)
-    return temp[0] if temp else None
+import re
+import fileinput
+import argparse
+import gzip
+import time
 
 def dictPrintSorted(dict, min, keylen=15):
     """print sorted dictionary by value """
     for key, value in sorted(dict.items(), key=lambda kv: kv[1], reverse=True):
         if value > min: print("{:<{}}".format(key,keylen), ' - ', value)
-
-def get_open_handler(file):
-     """get openhook for file"""
-     return (lambda file, mode: gzip.open(file, mode='rt')) if file.endswith(".gz") else open
 
 try:
     tic = time.perf_counter() # beginning time
@@ -30,14 +20,15 @@ try:
     args=parser.parse_args()
     myDict={}
 
-    for file_name in args.file:
-        for line in fileinput.input(file_name, openhook=get_open_handler(file_name)):
-            temp = findRe(line)
-            if temp: myDict[temp] = 1 if temp not in myDict else myDict[temp]+1
+    for line in fileinput.input(args.file, openhook=fileinput.hook_compressed):
+        if isinstance(line, (bytes, bytearray)): line = line.decode('UTF-8')
+        temp = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', line)
+        if temp: myDict[temp[0]] = 1 if temp[0] not in myDict else myDict[temp[0]] + 1
 
     dictPrintSorted(myDict, args.min)
+
+    toc = time.perf_counter() # end time
+    print(f"Processing {len(args.file)} filles - done in {toc - tic:0.4f} seconds")
+
 except Exception as e:
     print(e)
-
-toc = time.perf_counter() # end time
-print(f"Processing {len(args.file)} filles - done in {toc - tic:0.4f} seconds")
